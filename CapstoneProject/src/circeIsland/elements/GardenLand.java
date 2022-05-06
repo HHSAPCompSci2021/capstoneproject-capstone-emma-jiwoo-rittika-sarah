d6fb47a7a74f91f89691e48b05a64d2a633215a7
BuildTools <89165695+eyu028@users.noreply.github.com>
2022-05-06 15:15:10 -0700

    made jar + javadocs

---
+++ CapstoneProject/src/circeIsland/elements/GardenLand.java
@@ -0,0 +1,27 @@
+package circeIsland.elements;
+
+public class GardenLand extends Element{
+
+	private String name;
+	
+	//METHODS
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void grow() {
+		
+	}
+	
+	public void harvest() {
+		
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- CapstoneProject/src/circeIsland/elements/River.java
+++ CapstoneProject/src/circeIsland/elements/River.java
@@ -2,4 +2,10 @@ package circeIsland.elements;
 
 public class River extends Element{
 
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
