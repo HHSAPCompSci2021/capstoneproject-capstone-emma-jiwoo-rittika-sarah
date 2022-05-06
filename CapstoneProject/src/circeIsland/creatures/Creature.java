e19a189c4bc2677452ee95a661a2891a74528c8d
Jiwoo Kim <89165751+J1300o0@users.noreply.github.com>
2022-05-06 15:25:25 -0700

    Make checkGrid method

--- CapstoneProject/src/circeIsland/creatures/Creature.java
+++ CapstoneProject/src/circeIsland/creatures/Creature.java
@@ -1,32 +1,40 @@
 package circeIsland.creatures;
 
 import circeIsland.screens.Island;
+
+
 import circeIsland.elements.*;
 
 /**
  * 
  * @author Jiwoo Kim
  */
-public class Creature {
-	private int x, y;
+public abstract class Creature {
+	private double x, y;
 	private int xVelocity, yVelocity;
-	private Island myIsland;
 	
 	public Creature (int x, int y) {
 		this.x = x;
 		this.y = y;
+		xVelocity = 1;
+		yVelocity = 1;
 	}
 	
 	public void move() {
 		
 	}
 	
-	public Element checkGrid() {
+	public int[] checkGrid(Island myIsland) {
 		double width = myIsland.getWidth();
 		double height = myIsland.getHeight();
+		int xGrid = (int) (x/(width/10));
+		int yGrid = (int) (y/(height/10));
 		
-		return null;
+		int[] coor = {xGrid, yGrid};
+		return coor;
 	}
+	
+	public abstract void draw();
 	
 	
 	
