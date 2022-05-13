7862778ebd850bc2b99bf2387d39f1e6575146af
BuildTools <89165695+eyu028@users.noreply.github.com>
2022-05-13 11:12:23 -0700

    holdable class

--- CapstoneProject/src/circeIsland/elements/Holdable.java
+++ CapstoneProject/src/circeIsland/elements/Holdable.java
@@ -1,30 +1,43 @@
 package circeIsland.elements;
 
+import circeIsland.main.DrawingSurface;
+
 public class Holdable {
 	int type;
-	String details;
 	public static final int BREAD = 1;
 	public static final int WATER = 2;
-	public static final int SEED_GRAPE = 3;
-	public static final int SEED_BARLEY = 4;
-	public static final int SEED_MARATHO = 5;
-	public static final int SEED_ANITHOS = 6;
-	public static final int GRAPE = 7;
-	public static final int BARLEY = 8;
-	public static final int MARATHO = 9;
-	public static final int ANITHOS = 1;
-	public static final int POTION = 1;
+	public static final int SEED = 3;
+	public static final int PLANT = 4;
+	public static final int POTION = 5;
+	
+	String details; //only for seed and plant
+	public static final String GRAPE = "grape";
+	public static final String BARLEY = "barley";
+	public static final String MARATHOS = "marathos";
+	public static final String ANITHOS = "anithos";
+
+	
+
 
 	public Holdable(int type) {
-		
+		this.type = type;
 	}
 	
 	public Holdable(int type, String details) {
-		
+		this.type = type;
+		this.details = details;
 	}
 	
 	
 	public int getType() {
 		return type;
+	}
+	
+	public String getDetails() {
+		return details;
+	}
+	
+	public void draw(DrawingSurface surface, float cellWidth, float cellHeight) {
+		
 	}
 }
