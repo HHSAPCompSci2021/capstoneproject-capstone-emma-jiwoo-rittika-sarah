a851a8e919f6f916144883c9a67c31fc96538324
Jiwoo Kim <89165751+J1300o0@users.noreply.github.com>
2022-05-20 09:45:13 -0700

    Inventory Selecting

--- CapstoneProject/src/circeIsland/creatures/Circe.java
+++ CapstoneProject/src/circeIsland/creatures/Circe.java
@@ -84,16 +84,27 @@ public class Circe extends Creature{
 		
 		float screenWidth = super.getIsland().WIDTH;
 		float screenHeight = super.getIsland().HEIGHT;
-		float cell = screenHeight/20;
+		float cell = screenHeight/16;
 		float cellStartY =  (screenHeight - (holdings.length * cell))/2;
+		g.strokeWeight(7);
+		g.stroke(110,110,110);
 		for(int i = 0; i<holdings.length; i++) {
 			g.rect(screenWidth-cell-50,cellStartY + cell*i, cell, cell);
 //			holdings[i].draw(g, cellStartX + cell*i, screenHeight-cell-50);
 		}
+		g.stroke(255,205,0);
+		g.rect(screenWidth-cell-50,cellStartY + cell*currentHold, cell, cell);
 	}
 	
 	public int getInventoryByCoor (int x, int y) {
-		 return -1;
+		float screenWidth = super.getIsland().WIDTH;
+		float screenHeight = super.getIsland().HEIGHT;
+		float cell = screenHeight/20;
+		float cellStartY =  (screenHeight - (holdings.length * cell))/2;
+		if(screenWidth-cell-50 < x && x < screenWidth-50) {
+			return (int)((y-cellStartY)/cell);
+		}
+		return -1;
 	}
 	
 	public int nextEmptySpace() {
